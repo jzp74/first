1d0cc4435f80f2bab0a80f8f7e7a18be25117673
Jasper de Jong <jzp74@users.noreply.github.com>
2007-06-28 19:43:16 +0000

    Initial versions

---
+++ firstthingsfirst/php/Text.Buttons.php
@@ -0,0 +1,16 @@
+<?php
+
+
+# This file contains all buttons texts
+
+
+# shared buttons
+define("BUTTON_DELETE", "delete");
+define("BUTTON_ADD", "add");
+define("BUTTON_CANCEL", "cancel");
+define("BUTTON_BACK", "back");
+
+# Html.List buttons
+define("BUTTON_ADD_ROW", "add row");
+
+?>
---
+++ firstthingsfirst/php/Text.Labels.php
@@ -0,0 +1,20 @@
+<?php
+
+
+# This file contains all labels texts
+
+
+# shared labels
+define("LABEL_MINUS", "-");
+define("LABEL_CREATED_BY", "created by");
+define("LABEL_AT", "at");
+define("LABEL_LAST_MODIFICATION_BY", "last modification by");
+define("LABEL", "");
+define("LABEL", "");
+define("LABEL", "");
+
+# Html.List labels
+define("LABEL_PAGE", "page");
+define("LABEL_OF", "of");
+
+?>

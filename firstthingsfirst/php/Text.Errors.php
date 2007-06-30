25a9f43896c9ad856c5dcb56f53032ad4d35c44d
Jasper de Jong <jzp74@users.noreply.github.com>
2007-06-30 12:54:39 +0000

    Replaced all text occurrences with defines.

--- firstthingsfirst/php/Text.Errors.php
+++ firstthingsfirst/php/Text.Errors.php
@@ -7,6 +7,9 @@
 # field check errors
 define("ERROR_DATE_WRONG_FORMAT", "date format incorrect");
 define("ERROR_NO_VALUE_GIVEN", "please enter a value for this field");
+define("ERROR_NO_NAME_GIVEN", "please enter a name");
+define("ERROR_NO_PASSWORD_GIVEN", "please enter a password");
+define("ERROR_INCORRECT_NAME_PASSWORD", "name/password combination incorrect<br>please enter correct name and password");
 
 # database errors
 define("ERROR_DATABASE_PROBLEM", "database problem, please try again");

623bb4d68dd1ea1c7dc0259467967880cb6b2db5
Jasper <Jasper@f3b84c90-381b-4b63-94ea-c67ac0b4c3d5>
2007-09-09 14:04:02 +0000

    - changed text on action in list view - added navigation bar

--- trunk/firstthingsfirst/php/Html.Portal.php
+++ trunk/firstthingsfirst/php/Html.Portal.php
@@ -38,7 +38,10 @@ function action_get_portal_page ()
     $html_str .= "\n\n        <div id=\"hidden_upper_margin\">something to fill space</div>\n\n";
     $html_str .= "        <div id=\"page_title\">".$firstthingsfirst_portal_title."</div>\n\n";
     $html_str .= "        <div id=\"portal_explanation\"><em>".$firstthingsfirst_portal_intro_text."</em></div>\n\n";
-    $html_str .= "        <div id=\"login_status\">&nbsp;</div>\n\n";
+    $html_str .= "        <div id=\"navigation_container\">\n";
+    $html_str .= "            <div id=\"navigation\">&nbsp;</div>\n";
+    $html_str .= "            <div id=\"login_status\">&nbsp;</div>&nbsp\n";
+    $html_str .= "        </div> <!-- navigation_container -->\n\n";    
     $html_str .= "        <div id=\"portal_overview_pane\">\n\n";
 
     $result->set_result_str($html_str);    
